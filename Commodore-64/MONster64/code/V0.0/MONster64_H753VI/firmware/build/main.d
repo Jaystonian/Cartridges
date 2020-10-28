1c85b225f74f29b277c01fec01f5a8d8efb57f88
Jaystonian <46465588+Jaystonian@users.noreply.github.com>
2020-10-28 09:46:17 -0400

    Add files via upload

---
+++ Commodore-64/MONster64/code/V0.0/MONster64_H753VI/firmware/build/main.d
@@ -0,0 +1,155 @@
+build/main.o: main.c commands.h print.h stm32h7xx/hal.c \
+ stm32h7xx/cmsis/system_stm32h7xx.c stm32h7xx/cmsis/stm32h7xx.h \
+ stm32h7xx/cmsis/stm32h753xx.h stm32h7xx/cmsis/core_cm7.h \
+ stm32h7xx/cmsis/cmsis_version.h stm32h7xx/cmsis/cmsis_compiler.h \
+ stm32h7xx/cmsis/cmsis_gcc.h stm32h7xx/cmsis/mpu_armv7.h \
+ stm32h7xx/cmsis/system_stm32h7xx.h stm32h7xx/hal.h \
+ stm32h7xx/c64_interface.c stm32h7xx/diskio.c fatfs/ff.h fatfs/ffconf.h \
+ fatfs/diskio.h stm32h7xx/usb.c stm32h7xx/cmsis/stm32.h \
+ stm32h7xx/libusb_stm32/usb.h stm32h7xx/libusb_stm32/usbd_core.h \
+ stm32h7xx/libusb_stm32/usb_std.h stm32h7xx/libusb_stm32/usb_cdc.h \
+ stm32h7xx/libusb_stm32/usb_hid.h \
+ stm32h7xx/libusb_stm32/hid_usage_desktop.h \
+ stm32h7xx/libusb_stm32/hid_usage_button.h \
+ stm32h7xx/libusb_stm32/usbd_core.c stm32h7xx/libusb_stm32/usb.h \
+ stm32h7xx/libusb_stm32/usbd_stm32h7x3_otgfs.c stm32h7xx/flash.c print.c \
+ filesystem.c file_types.c file_types.h d64_reader.h \
+ cartridges/cartridge.c cartridges/cartridge.h cartridges/crt_normal.c \
+ cartridges/action_replay_4x.c cartridges/kcs_power_cartridge.c \
+ cartridges/final_cartridge_3.c cartridges/simons_basic.c \
+ cartridges/epyx_fastload.c cartridges/c64gs_system_3.c \
+ cartridges/dinamic.c cartridges/zaxxon.c cartridges/magic_desk.c \
+ cartridges/super_snapshot_5.c cartridges/easyflash.c \
+ cartridges/comal80.c commands.c menu.c menu.h menu_sd.h menu_d64.h \
+ menu_options.h d64_reader.c loader.c menu_sd.c menu_d64.c menu_options.c \
+ menu_settings.c disk_drive.c disk_drive.h
+
+commands.h:
+
+print.h:
+
+stm32h7xx/hal.c:
+
+stm32h7xx/cmsis/system_stm32h7xx.c:
+
+stm32h7xx/cmsis/stm32h7xx.h:
+
+stm32h7xx/cmsis/stm32h753xx.h:
+
+stm32h7xx/cmsis/core_cm7.h:
+
+stm32h7xx/cmsis/cmsis_version.h:
+
+stm32h7xx/cmsis/cmsis_compiler.h:
+
+stm32h7xx/cmsis/cmsis_gcc.h:
+
+stm32h7xx/cmsis/mpu_armv7.h:
+
+stm32h7xx/cmsis/system_stm32h7xx.h:
+
+stm32h7xx/hal.h:
+
+stm32h7xx/c64_interface.c:
+
+stm32h7xx/diskio.c:
+
+fatfs/ff.h:
+
+fatfs/ffconf.h:
+
+fatfs/diskio.h:
+
+stm32h7xx/usb.c:
+
+stm32h7xx/cmsis/stm32.h:
+
+stm32h7xx/libusb_stm32/usb.h:
+
+stm32h7xx/libusb_stm32/usbd_core.h:
+
+stm32h7xx/libusb_stm32/usb_std.h:
+
+stm32h7xx/libusb_stm32/usb_cdc.h:
+
+stm32h7xx/libusb_stm32/usb_hid.h:
+
+stm32h7xx/libusb_stm32/hid_usage_desktop.h:
+
+stm32h7xx/libusb_stm32/hid_usage_button.h:
+
+stm32h7xx/libusb_stm32/usbd_core.c:
+
+stm32h7xx/libusb_stm32/usb.h:
+
+stm32h7xx/libusb_stm32/usbd_stm32h7x3_otgfs.c:
+
+stm32h7xx/flash.c:
+
+print.c:
+
+filesystem.c:
+
+file_types.c:
+
+file_types.h:
+
+d64_reader.h:
+
+cartridges/cartridge.c:
+
+cartridges/cartridge.h:
+
+cartridges/crt_normal.c:
+
+cartridges/action_replay_4x.c:
+
+cartridges/kcs_power_cartridge.c:
+
+cartridges/final_cartridge_3.c:
+
+cartridges/simons_basic.c:
+
+cartridges/epyx_fastload.c:
+
+cartridges/c64gs_system_3.c:
+
+cartridges/dinamic.c:
+
+cartridges/zaxxon.c:
+
+cartridges/magic_desk.c:
+
+cartridges/super_snapshot_5.c:
+
+cartridges/easyflash.c:
+
+cartridges/comal80.c:
+
+commands.c:
+
+menu.c:
+
+menu.h:
+
+menu_sd.h:
+
+menu_d64.h:
+
+menu_options.h:
+
+d64_reader.c:
+
+loader.c:
+
+menu_sd.c:
+
+menu_d64.c:
+
+menu_options.c:
+
+menu_settings.c:
+
+disk_drive.c:
+
+disk_drive.h:
---
+++ Commodore-64/MONster64/code/V0.0/MONster64_H753VI/firmware/build/startup_stm32h753vitx.d
@@ -0,0 +1,1 @@
+build/startup_stm32h753vitx.o: stm32h7xx/startup_stm32h753vitx.s
